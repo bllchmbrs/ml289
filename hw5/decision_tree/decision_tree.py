643721875bd741ccd12cdaef1ffebdd1b98c6ba5
anabranch <wac.chambers@gmail.com>
2015-11-05 11:14:13 -0800

    removed pycache stuff, working decision tree

--- hw5/decision_tree/decision_tree.py
+++ hw5/decision_tree/decision_tree.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import entropy, itemfreq
+from scipy.stats import entropy, itemfreq, mode
 from numpy.random import choice
 
 
@@ -54,20 +54,20 @@ class DecisionTree:
                     "impurity": self.impurity(pc1, pc2)
                 })
         best = min(impurities, key=lambda x: x['impurity'])
-        print(best)
+        #        print(best)
         return best['feature'], best['split']
 
     def train(self, train_data, train_labels):  # could add random seed
         if not self.pretrain_check(train_data, train_labels):
             return  # don't pass the check, don't train
-        print(quick_stats(train_data))
+#        print(quick_stats(train_data))
 
         shuff = choice(len(train_labels), len(train_labels))
         train_data = train_data[shuff]
         train_labels = train_labels[shuff]
 
         feature, split = self.segmentor(train_data, train_labels)
-        self.node = Node(feature, split)
+        self.node = Node(feature, split, mode(train_labels).mode[0])
         ldata, rdata, llabels, rlabels = self.node.apply(train_data,
                                                          train_labels)
 
@@ -77,35 +77,51 @@ class DecisionTree:
         self.node.right = DecisionTree(
             {"max_depth": self.max_depth - 1,
              "min_points": self.min_points})
-        print("training left node - depth: %i" % (4 - self.max_depth))
+        #       print("training left node - depth: %i" % (4 - self.max_depth))
         self.node.left.train(ldata, llabels)
-        print("training right node - depth: %i" % (4 - self.max_depth))
+        #        print("training right node - depth: %i" % (4 - self.max_depth))
         self.node.right.train(rdata, rlabels)
 
     def predict(self, test_data):
-        pass
+        return np.array([self.node.predict(row.reshape(1, len(row)))
+                         for row in test_data])
+
+    def score(self, test_data, answers):
+        return self.predict(test_data) == answers
 
 
 class Node:
     def __init__(self, feature, split_rule, label=None):
-        self.parent = None
-        self.feature = feature  # feature number
+        self.feature = feature
         self.split_rule = split_rule
         self.label = label
-        self.left = None
-        self.right = None
+        self.left = None  # will be of type DecisionTree
+        self.right = None  # will be of type DecisionTree
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "Feature: %i, Rule: %.3f" % (self.feature, self.split_rule)
+        return "Feature: %i, Rule: %.3f, label: %i" % (self.feature,
+                                                       self.split_rule,
+                                                       self.label)
 
-    def apply(self, data, labels=None):
+    def apply(self, data, labels):
         left = np.where(data[:, self.feature] <= self.split_rule)
         right = np.where(data[:, self.feature] > self.split_rule)
+        return data[left], data[right], labels[left], labels[right]
 
-        if type(labels) != type(None):
-            return data[left], data[right], labels[left], labels[right]
+    def predict(self, row):
+        left = np.where(row[:, self.feature] <= self.split_rule)
+        right = np.where(row[:, self.feature] > self.split_rule)
+        # print(self, "making a prediction")
+        # print("Go left: %i, Go Right: %i" % (len(row[left]), len(row[right])))
+        # print("Has left: %i, Has Right: %i" %
+        #       (self.left.node != None, self.right.node != None))
 
-        return data[left], data[right]
+        if len(row[left]) == 0 and self.right.node != None:
+            return self.right.node.predict(row)
+        if len(row[right]) == 0 and self.left.node != None:
+            return self.left.node.predict(row)
+        else:
+            return self.label
